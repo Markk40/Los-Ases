36c42101170b43095329c27db1bbd41863a44e4c
Jorgemoraa <jorgemoratallavita@gmail.com>
2025-02-25 10:09:05 +0100

    created files

---
+++ Sprint2/los-ases-app/components/header.js
@@ -0,0 +1,15 @@
+import styles from './header.module.css';
+export default function Header() {
+  return (
+    <header className={styles.navbar}>
+      <a href="/" className={styles.logo}>
+        <img src="/logoAses2.jpg" alt="Logo Los Ases" />
+      </a>
+      <h1 className={styles.companyName}>LOS ASES</h1>
+      <div className={styles.authButtons}>
+        <button className={styles.loginBtn}><Link href="/inicio">Log In</Link></button>
+        <button className={styles.signupBtn}><Link href="/registro">Sign Up</Link></button>
+      </div>
+      </header>
+  );
+}
