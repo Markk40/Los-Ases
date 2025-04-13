1c6497aa1318a670efefb5391c997638a1da3237
Marcos <202202382@alu.comillas.com>
2025-04-13 19:46:11 +0200

    error subastas corregido

--- Sprint3/los-ases-app/app/utils/api.js
+++ Sprint3/los-ases-app/app/utils/api.js
@@ -112,7 +112,14 @@ export const getBidsByAuction = async (auctionId) => {
 };
 
 export const getAllBids = async () => {
-  const res = await fetch("http://localhost:8000/api/bids/");
-  if (!res.ok) throw new Error("Error al obtener pujas");
-  return res.json();
+  try {
+    const res = await fetch("http://localhost:8000/api/bids/");
+    if (!res.ok) {
+      throw new Error("Error al obtener pujas");
+    }
+    return await res.json();
+  } catch (error) {
+    // console.error("❌ Error en getAllBids:", error.message);
+    return []; // devolvemos un array vacío si hay fallo
+  }
 };
