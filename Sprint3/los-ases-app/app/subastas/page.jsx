84f4abfac8900af648e0dfd303e980001c4d872f
Jorgemoraa <jorgemoratallavita@gmail.com>
2025-05-13 21:02:43 +0200

    fixed url error

--- Sprint3/los-ases-app/app/subastas/page.jsx
+++ Sprint3/los-ases-app/app/subastas/page.jsx
@@ -38,10 +38,13 @@ export default function SearchResults() {
   // carga subastas cada vez que cambian los filtros de backend
   useEffect(() => {
     const params = new URLSearchParams();
-    if (onlyOpen) params.set("is_open", "true");
-    if (ratingMin) params.set("rating_min", ratingMin);
-    console.log("Fetch auctions:", url);
-    fetch(`${API_BASE_URL}?${params.toString()}`)
+  if (onlyOpen)   params.set("is_open", "true");
+  if (ratingMin)  params.set("rating_min", ratingMin);
+
+  const queryString = params.toString();
+  const fetchUrl = `${API_BASE_URL}${queryString ? `?${queryString}` : ""}`;
+  console.log("Fetch auctions:", fetchUrl);
+    fetch(fetchUrl)
       .then(r => {
         if (!r.ok) throw new Error("Error al cargar subastas");
         return r.json();
